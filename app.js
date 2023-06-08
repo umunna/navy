0435fc8cece3068d2b2d2a4063929e6ac4b477e9
nkTheDiligent <kingsleyumunna9@gmail.com>
2023-06-08 20:52:25 +0200

    Add files via upload
    friendly responsive navbar.

---
+++ app.js
@@ -0,0 +1,14 @@
+// classList - shows/gets all classes
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+
+// toggle - toggles class 
+navToggle.addEventListener("click", function() {
+  links.classList.toggle("show-links");
+  });
+
+
+
+
+
